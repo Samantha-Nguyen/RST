a70bd5a94b276b5f3834a77a5adb73c5056572a0
SamanthaNguyen1 <>
2021-05-17 04:21:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// This connects the "START" button to the play function
+document.getElementById('start').addEventListener('click', play)
+document.getElementById('enter').addEventListener('click', enter)
+
+// This brings the user to another page (index2.html) when the "START" button is clicked
+function play () {
+  window.location.href = 'index2.html'
+}
+
+function enter () {
+  document.body.style.backgroundColor = "#39f"
+}
